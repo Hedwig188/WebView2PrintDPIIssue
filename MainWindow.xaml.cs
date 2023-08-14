6fb4a16fc271ae3757cd1a33e174840de29d6f9f
Hedwig188 <yawei.huang188@gmail.com>
2023-08-14 13:30:20 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.Windows;
+
+namespace WebView2PrintDPIIssue
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			Loaded += MainWindow_Loaded;
+		}
+
+		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			InitializeWebView();
+		}
+
+		private async void InitializeWebView()
+		{
+			await webView.EnsureCoreWebView2Async();
+			webView.CoreWebView2.NavigationCompleted += PrintDocument;
+		}
+
+		private void PrintDocument(object sender, CoreWebView2NavigationCompletedEventArgs args)
+		{
+			CoreWebView2PrintSettings printSetting = CreatePrinterSetting("Adobe PDF");
+			webView.CoreWebView2.PrintAsync(printSetting);
+		}
+
+		private CoreWebView2PrintSettings CreatePrinterSetting(string printerName)
+		{
+			CoreWebView2PrintSettings printerSettings = webView.CoreWebView2.Environment.CreatePrintSettings();
+			printerSettings.PrinterName = printerName;
+			return printerSettings;
+		}
+	}
+}
